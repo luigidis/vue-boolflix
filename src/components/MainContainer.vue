f86292f8fa527e302498e5d5cc89e60c97ea4ee6
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-12 15:23:29 +0200

    Milestone 2

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="main container py-5">
     <input type="text" v-model="query">
-    <button class="ms-2" @click="searchMovies">Cerca!</button>
+    <button class="ms-2" @click="clickSearch">Cerca!</button>
+    <h2>Films</h2>
     <ul>
       <li v-for="movie in movies" :key="movie.id">
         <div>Titolo:{{ movie.title }}</div>
@@ -11,6 +12,18 @@
         </div>
         <div>Voto: {{ movie.vote_average}}</div>
       </li>
+    </ul>
+    <h2>Series</h2>
+    <ul class="series_container">
+      <li v-for="serie in series" :key="serie.id">
+        <div>Titolo: {{ serie.name }}</div>
+        <div>Titolo Originale: {{ serie.original_name}}</div>
+        <div>Lingua:
+          <lang-flag :iso="serie.original_language" :squared="false" />
+        </div>
+        <div>Voto: {{ serie.vote_average }}</div>
+      </li>
+
     </ul>
   </div>
 </template>
@@ -28,6 +41,7 @@ export default {
   data() {
     return {
       movies: [],
+      series: [],
       api_key: '7b5d3ffe89177bc82927826d88ed3293',
       query: '',
       BASE_URL: 'https://api.themoviedb.org/3',
@@ -48,14 +62,37 @@ export default {
           // console.log(res)
           this.movies = res.data.results
         })
+    },
+    searchSeries() {
+      if (this.query.trim() === '')
+        return
+      axios
+        .get(`${this.BASE_URL}/search/tv`, {
+          params: {
+            api_key: this.api_key,
+            query: this.query,
+          }
+        })
+        .then((res) => {
+          // console.log(res)
+          this.series = res.data.results
+        })
+    },
+    clickSearch() {
+       this.searchMovies();
+       this.searchSeries();
     }
   },
+    
   created() {
     if (this.query) {
-      this.searchMovies()
+      this.clickSearch()
     }
   }
 }
+      
+
+
 
 </script>
 
